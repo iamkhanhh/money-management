213c0761e6a8d194d6c8f7d10fd67d0351809d6f
Nguyen Khanh <khanh9102004@gmail.com>
2024-05-18 10:38:14 +0700

    docker done

--- src/app.module.ts
+++ src/app.module.ts
@@ -22,8 +22,9 @@ import { MailModule } from './mail/mail.module';
     }),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: process.env.DB_HOST,
-      port: 3306, 
+      // host: process.env.DB_HOST,
+      host: 'host.docker.internal',
+      port: 3306,  
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
